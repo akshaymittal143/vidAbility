7b0f5f0977ee8e26dccdafa781cba1e80e72eac6
Arthur Jones <arthrjns@outlook.com>
2018-07-15 10:41:50 -0500

    Watson edits

--- client/assets/js/watson.js
+++ client/assets/js/watson.js
@@ -8,14 +8,25 @@ fetch('/api/speech-to-text/token')
       outputElement: '#output' // CSS selector or DOM Element
     });
     stream.on('data', function(t) {
-      console.log(t.results[0].alternatives[0].transcript.includes('search for'))
-      console.log(t.results[0].alternatives[0].transcript)
       var text = t.results[0].alternatives[0].transcript.toLowerCase();
-      console.log(text.includes('search for'))
+      console.log(text)
       if (text.includes('search for')) {
         var edited = text.split("search for").slice(1).join(' ');
-        console.log(edited)
         $('.search-container .form-control').val(edited)
+      }
+      if(text.includes('scroll down')){
+        var y = $(window).scrollTop();  //your current y position on the page
+        $('html, body').animate({scrollTop: '+=150px'}, 300);
+      }
+      if(text.includes('scroll up')){
+        var y = $(window).scrollTop();  //your current y position on the page
+        $('html, body').animate({scrollTop: '-=150px'}, 300);
+      }
+      if(text.includes('join video chat')){
+        window.location.href = "https://localhost:8443/video-chat";
+      }
+      if(text.includes('log in')){
+        window.location.href = "https://localhost:8443/my-account.html";
       }
     })
     stream.on('error', function(err) {
